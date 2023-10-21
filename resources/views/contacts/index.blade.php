2d7646199591869227bb7fb89584d8d0e1aeae4a
inomoto daiya <daiandaian2525@gmail.com>
2023-10-22 03:10:30 +0900

    ✨ Create ContactForm show function

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -9,8 +9,39 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    index<br/>
+                    index<br />
                     <a href="{{ route('contacts.create') }}" class="text-blue-500 mt-2 block">新規作成</a>
+
+                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                        <table class="table-auto w-full text-left whitespace-no-wrap mt-[40px]">
+                            <thead>
+                                <tr>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100 rounded-tl rounded-bl">
+                                        id</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        名前</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        件名</th>
+                                    <th
+                                        class="px-4 py-3 title-font tracking-wider font-medium text-gray-900 text-sm bg-gray-100">
+                                        作成日</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($contacts as $contact)
+                                    <tr>
+                                        <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->id }}</td>
+                                        <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->name }}</td>
+                                        <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->title }}</td>
+                                        <td class="border-t-2 border-gray-200 px-4 py-3 text-lg">{{ $contact->created_at }}</td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
