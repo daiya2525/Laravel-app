ccac03d051c7b59e4a8698d6692d3adf104d0bed
inomoto daiya <daiandaian2525@gmail.com>
2023-10-22 03:19:19 +0900

    💄 Improve style contacts page

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -9,10 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    index<br />
-                    <a href="{{ route('contacts.create') }}" class="text-blue-500 mt-2 block">新規作成</a>
+                    <a type="button" href='{{ route('contacts.create') }}'
+                        class="text-white bg-blue-400 dark:bg-blue-500 hover:opacity-70 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-[fit-content] ml-auto block"
+                        disabled>新規作成</a>
 
-                    <div class="lg:w-2/3 w-full mx-auto overflow-auto">
+                    <div class="w-full mx-auto overflow-auto">
                         <table class="table-auto w-full text-left whitespace-no-wrap mt-[40px]">
                             <thead>
                                 <tr>
@@ -36,7 +37,8 @@
                                         <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->id }}</td>
                                         <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->name }}</td>
                                         <td class="border-t-2 border-gray-200 px-4 py-3">{{ $contact->title }}</td>
-                                        <td class="border-t-2 border-gray-200 px-4 py-3 text-lg">{{ $contact->created_at }}</td>
+                                        <td class="border-t-2 border-gray-200 px-4 py-3 text-lg">
+                                            {{ $contact->created_at }}</td>
                                     </tr>
                                 @endforeach
                             </tbody>
